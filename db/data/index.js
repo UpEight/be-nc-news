0fcb3b8a1370906bf63adeea97cf2910ae36e269
UpEight <milnerslm@gmail.com>
2019-10-07 11:29:16 +0100

    Setup project db/data/ with 3 index.js files to manage data exports for environments

---
+++ db/data/index.js
@@ -0,0 +1,11 @@
+const ENV = process.env.NODE_ENV || "development";
+
+const development_data = require("./development-data");
+const test_data = require("./test-data");
+
+const data = {
+  development: development_data,
+  test: test_data
+};
+
+module.exports = data[ENV];
