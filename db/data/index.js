55f980e8bec678d898872fdd9fc15911f9771afb
UpEight <milnerslm@gmail.com>
2019-10-23 10:07:12 +0100

    Add production config to enable Heroku hosting

--- db/data/index.js
+++ db/data/index.js
@@ -1,11 +1,12 @@
-const ENV = process.env.NODE_ENV || "development";
+const ENV = process.env.NODE_ENV || 'development';
 
-const development_data = require("./development-data");
-const test_data = require("./test-data");
+const development_data = require('./development-data');
+const test_data = require('./test-data');
 
 const data = {
   development: development_data,
-  test: test_data
+  test: test_data,
+  production: development_data
 };
 
 module.exports = data[ENV];
