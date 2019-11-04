b5842d417a8cfafa7179c969b089fc3b8659f1b4
UpEight <milnerslm@gmail.com>
2019-11-04 12:32:26 +0000

    Remove console.log from api-controller

--- controllers/api-controller.js
+++ controllers/api-controller.js
@@ -1,7 +1,6 @@
 const { getApi } = require('../models/api-model');
 
 exports.sendApi = (req, res, next) => {
-  console.log('In th api model');
   const api = getApi();
   res.status(200).send(JSON.stringify(api));
 };
