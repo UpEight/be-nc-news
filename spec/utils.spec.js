8722e2a47901f0ce12be728c0521663bd6081d46
UpEight <milnerslm@gmail.com>
2019-11-04 12:33:27 +0000

    Fix issue with utility function being required from deleted location

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,14 +1,13 @@
-const { expect } = require("chai");
+const { expect } = require('chai');
 const {
   formatDates,
   makeRefObj,
-  formatComments
-} = require("../db/utils/utils");
+  formatComments,
+  formatPostedComment
+} = require('../db/utils/utils');
 
-const { formatPostedComment } = require("../utils/utils");
-
-describe("formatDates", () => {
-  it("returns a new empty array when passed an empty array", () => {
+describe('formatDates', () => {
+  it('returns a new empty array when passed an empty array', () => {
     const actual = formatDates([]);
     const expected = [];
     expect(actual).to.eql(expected);
@@ -17,20 +16,20 @@ describe("formatDates", () => {
   it("returns an array containing 1 object with the value of 'created_at' converted to a JavaScript Date object when passed an array of 1 object", () => {
     const articles = [
       {
-        title: "Living in the shadow of a great man",
-        topic: "mitch",
-        author: "butter_bridge",
-        body: "I find this existence challenging",
+        title: 'Living in the shadow of a great man',
+        topic: 'mitch',
+        author: 'butter_bridge',
+        body: 'I find this existence challenging',
         created_at: 1542284514171,
         votes: 100
       }
     ];
     const expected = [
       {
-        title: "Living in the shadow of a great man",
-        topic: "mitch",
-        author: "butter_bridge",
-        body: "I find this existence challenging",
+        title: 'Living in the shadow of a great man',
+        topic: 'mitch',
+        author: 'butter_bridge',
+        body: 'I find this existence challenging',
         created_at: new Date(1542284514171),
         votes: 100
       }
@@ -41,49 +40,49 @@ describe("formatDates", () => {
   it("returns an array of article objects with the value of 'created_at' converted to a JavaScript Date object when passed an array of multiple articles", () => {
     const articles = [
       {
-        title: "Living in the shadow of a great man",
-        topic: "mitch",
-        author: "butter_bridge",
-        body: "I find this existence challenging",
+        title: 'Living in the shadow of a great man',
+        topic: 'mitch',
+        author: 'butter_bridge',
+        body: 'I find this existence challenging',
         created_at: 1542284514171,
         votes: 100
       },
       {
-        title: "Sony Vaio; or, The Laptop",
-        topic: "mitch",
-        author: "icellusedkars",
-        body: "Call me Mitchell.",
+        title: 'Sony Vaio; or, The Laptop',
+        topic: 'mitch',
+        author: 'icellusedkars',
+        body: 'Call me Mitchell.',
         created_at: 1416140514171
       },
       {
-        title: "Eight pug gifs that remind me of mitch",
-        topic: "mitch",
-        author: "icellusedkars",
-        body: "some gifs",
+        title: 'Eight pug gifs that remind me of mitch',
+        topic: 'mitch',
+        author: 'icellusedkars',
+        body: 'some gifs',
         created_at: 1289996514171
       }
     ];
     const expected = [
       {
-        title: "Living in the shadow of a great man",
-        topic: "mitch",
-        author: "butter_bridge",
-        body: "I find this existence challenging",
+        title: 'Living in the shadow of a great man',
+        topic: 'mitch',
+        author: 'butter_bridge',
+        body: 'I find this existence challenging',
         created_at: new Date(1542284514171),
         votes: 100
       },
       {
-        title: "Sony Vaio; or, The Laptop",
-        topic: "mitch",
-        author: "icellusedkars",
-        body: "Call me Mitchell.",
+        title: 'Sony Vaio; or, The Laptop',
+        topic: 'mitch',
+        author: 'icellusedkars',
+        body: 'Call me Mitchell.',
         created_at: new Date(1416140514171)
       },
       {
-        title: "Eight pug gifs that remind me of mitch",
-        topic: "mitch",
-        author: "icellusedkars",
-        body: "some gifs",
+        title: 'Eight pug gifs that remind me of mitch',
+        topic: 'mitch',
+        author: 'icellusedkars',
+        body: 'some gifs',
         created_at: new Date(1289996514171)
       }
     ];
@@ -91,83 +90,83 @@ describe("formatDates", () => {
     expect(actual).to.eql(expected);
   });
 
-  it("does not mutate the input array", () => {
+  it('does not mutate the input array', () => {
     const input = [
       {
-        title: "Living in the shadow of a great man",
-        topic: "mitch",
-        author: "butter_bridge",
-        body: "I find this existence challenging",
+        title: 'Living in the shadow of a great man',
+        topic: 'mitch',
+        author: 'butter_bridge',
+        body: 'I find this existence challenging',
         created_at: 1542284514171,
         votes: 100
       },
       {
-        title: "Sony Vaio; or, The Laptop",
-        topic: "mitch",
-        author: "icellusedkars",
-        body: "Call me Mitchell.",
+        title: 'Sony Vaio; or, The Laptop',
+        topic: 'mitch',
+        author: 'icellusedkars',
+        body: 'Call me Mitchell.',
         created_at: 1416140514171
       },
       {
-        title: "Eight pug gifs that remind me of mitch",
-        topic: "mitch",
-        author: "icellusedkars",
-        body: "some gifs",
+        title: 'Eight pug gifs that remind me of mitch',
+        topic: 'mitch',
+        author: 'icellusedkars',
+        body: 'some gifs',
         created_at: 1289996514171
       }
     ];
     formatDates(input);
     expect(input).to.eql([
       {
-        title: "Living in the shadow of a great man",
-        topic: "mitch",
-        author: "butter_bridge",
-        body: "I find this existence challenging",
+        title: 'Living in the shadow of a great man',
+        topic: 'mitch',
+        author: 'butter_bridge',
+        body: 'I find this existence challenging',
         created_at: 1542284514171,
         votes: 100
       },
       {
-        title: "Sony Vaio; or, The Laptop",
-        topic: "mitch",
-        author: "icellusedkars",
-        body: "Call me Mitchell.",
+        title: 'Sony Vaio; or, The Laptop',
+        topic: 'mitch',
+        author: 'icellusedkars',
+        body: 'Call me Mitchell.',
         created_at: 1416140514171
       },
       {
-        title: "Eight pug gifs that remind me of mitch",
-        topic: "mitch",
-        author: "icellusedkars",
-        body: "some gifs",
+        title: 'Eight pug gifs that remind me of mitch',
+        topic: 'mitch',
+        author: 'icellusedkars',
+        body: 'some gifs',
         created_at: 1289996514171
       }
     ]);
   });
 });
 
-describe("makeRefObj", () => {
-  it("returns a new empty object when passed an empty array", () => {
+describe('makeRefObj', () => {
+  it('returns a new empty object when passed an empty array', () => {
     const actual = makeRefObj([]);
     const expected = {};
     expect(actual).to.eql(expected);
   });
-  it("returns an object containing the title and article id as a key-value pair when passed an array containing 1 article object", () => {
-    const actual = makeRefObj([{ article_id: 1, title: "A" }]);
+  it('returns an object containing the title and article id as a key-value pair when passed an array containing 1 article object', () => {
+    const actual = makeRefObj([{ article_id: 1, title: 'A' }]);
     const expected = { A: 1 };
     expect(actual).to.eql(expected);
   });
-  it("returns an object containing the title and article id as key-value pairs when passed an array containing multiple article objects", () => {
+  it('returns an object containing the title and article id as key-value pairs when passed an array containing multiple article objects', () => {
     const actual = makeRefObj([
-      { article_id: 1, title: "A" },
-      { article_id: 2, title: "B" },
-      { article_id: 3, title: "C" }
+      { article_id: 1, title: 'A' },
+      { article_id: 2, title: 'B' },
+      { article_id: 3, title: 'C' }
     ]);
     const expected = { A: 1, B: 2, C: 3 };
     expect(actual).to.eql(expected);
   });
 });
 
-describe("formatComments", () => {
-  it("returns a new empty array when passed an empty array and and a reference object", () => {
+describe('formatComments', () => {
+  it('returns a new empty array when passed an empty array and and a reference object', () => {
     const comments = [];
     const refObj = { A: 1, B: 2, C: 3 };
     const actual = formatComments(comments, refObj);
@@ -175,69 +174,69 @@ describe("formatComments", () => {
     expect(actual).to.eql(expected);
     expect(actual).to.not.equal(comments);
   });
-  it("returns a new array containing a new comments object with the `created_by` property renamed to an `author` key when passed an array containing 1 comment object", () => {
+  it('returns a new array containing a new comments object with the `created_by` property renamed to an `author` key when passed an array containing 1 comment object', () => {
     const comments = [
       {
-        created_by: "icellusedkars"
+        created_by: 'icellusedkars'
       }
     ];
     const refObj = {
       A: 1,
-      "Living in the shadow of a great man": 2,
+      'Living in the shadow of a great man': 2,
       "They're not exactly dogs, are they?": 3
     };
     const actual = formatComments(comments, refObj);
     const expected = [
       {
-        author: "icellusedkars"
+        author: 'icellusedkars'
       }
     ];
     expect(actual).to.eql(expected);
     expect(actual).to.not.equal(comments);
     expect(comments).to.eql([
       {
-        created_by: "icellusedkars"
+        created_by: 'icellusedkars'
       }
     ]);
   });
-  it("returns a new array containing new comments objects with the `created_by` property renamed to an `author` key when passed an array containing multiple comments objects", () => {
+  it('returns a new array containing new comments objects with the `created_by` property renamed to an `author` key when passed an array containing multiple comments objects', () => {
     const comments = [
       {
-        created_by: "icellusedkars"
+        created_by: 'icellusedkars'
       },
       {
-        created_by: "butter_bridge"
+        created_by: 'butter_bridge'
       },
       {
-        created_by: "icellusedkars"
+        created_by: 'icellusedkars'
       }
     ];
     const refObj = {
       A: 1,
-      "Living in the shadow of a great man": 2,
+      'Living in the shadow of a great man': 2,
       "They're not exactly dogs, are they?": 3
     };
     const actual = formatComments(comments, refObj);
     const expected = [
       {
-        author: "icellusedkars"
+        author: 'icellusedkars'
       },
       {
-        author: "butter_bridge"
+        author: 'butter_bridge'
       },
       {
-        author: "icellusedkars"
+        author: 'icellusedkars'
       }
     ];
     expect(actual).to.eql(expected);
   });
-  it("returns a new array containing new comments objects with the belongs_to` property renamed to an `article_id` key when passed an array containing multiple comments objects", () => {
+  it('returns a new array containing new comments objects with the belongs_to` property renamed to an `article_id` key when passed an array containing multiple comments objects', () => {
     const comments = [
       {
-        belongs_to: "Living in the shadow of a great man"
+        belongs_to: 'Living in the shadow of a great man'
       },
       {
-        belongs_to: "A"
+        belongs_to: 'A'
       },
       {
         belongs_to: "They're not exactly dogs, are they?"
@@ -245,19 +244,19 @@ describe("formatComments", () => {
     ];
     const refObj = {
       A: 1,
-      "Living in the shadow of a great man": 2,
+      'Living in the shadow of a great man': 2,
       "They're not exactly dogs, are they?": 3
     };
     const actual = formatComments(comments, refObj);
-    expect(Object.keys(actual[0])).to.eql(["article_id"]);
+    expect(Object.keys(actual[0])).to.eql(['article_id']);
   });
-  it("returns a new array containing new comments objects with the belongs_to` property changed to the `article_id` when passed an array containing multiple comments objects", () => {
+  it('returns a new array containing new comments objects with the belongs_to` property changed to the `article_id` when passed an array containing multiple comments objects', () => {
     const comments = [
       {
-        belongs_to: "Living in the shadow of a great man"
+        belongs_to: 'Living in the shadow of a great man'
       },
       {
-        belongs_to: "A"
+        belongs_to: 'A'
       },
       {
         belongs_to: "They're not exactly dogs, are they?"
@@ -265,7 +264,7 @@ describe("formatComments", () => {
     ];
     const refObj = {
       A: 1,
-      "Living in the shadow of a great man": 2,
+      'Living in the shadow of a great man': 2,
       "They're not exactly dogs, are they?": 3
     };
     const actual = formatComments(comments, refObj);
@@ -282,26 +281,26 @@ describe("formatComments", () => {
     ];
     expect(actual).to.eql(expected);
   });
-  it("handles being passed an empty reference object", () => {
+  it('handles being passed an empty reference object', () => {
     const comments = [
       {
-        body: "This is a bad article name",
-        belongs_to: "A",
-        created_by: "butter_bridge",
+        body: 'This is a bad article name',
+        belongs_to: 'A',
+        created_by: 'butter_bridge',
         votes: 1,
         created_at: 1038314163389
       },
       {
-        body: "The owls are not what they seem.",
+        body: 'The owls are not what they seem.',
         belongs_to: "They're not exactly dogs, are they?",
-        created_by: "icellusedkars",
+        created_by: 'icellusedkars',
         votes: 20,
         created_at: 1006778163389
       },
       {
-        body: "This morning, I showered for nine minutes.",
-        belongs_to: "Living in the shadow of a great man",
-        created_by: "butter_bridge",
+        body: 'This morning, I showered for nine minutes.',
+        belongs_to: 'Living in the shadow of a great man',
+        created_by: 'butter_bridge',
         votes: 16,
         created_at: 975242163389
       }
@@ -314,92 +313,92 @@ describe("formatComments", () => {
   it("converts each comment's 'created_at' value into a JavaScript Date object", () => {
     const comments = [
       {
-        body: "This is a bad article name",
-        belongs_to: "A",
-        created_by: "butter_bridge",
+        body: 'This is a bad article name',
+        belongs_to: 'A',
+        created_by: 'butter_bridge',
         votes: 1,
         created_at: 1038314163389
       },
       {
-        body: "The owls are not what they seem.",
+        body: 'The owls are not what they seem.',
         belongs_to: "They're not exactly dogs, are they?",
-        created_by: "icellusedkars",
+        created_by: 'icellusedkars',
         votes: 20,
         created_at: 1006778163389
       },
       {
-        body: "This morning, I showered for nine minutes.",
-        belongs_to: "Living in the shadow of a great man",
-        created_by: "butter_bridge",
+        body: 'This morning, I showered for nine minutes.',
+        belongs_to: 'Living in the shadow of a great man',
+        created_by: 'butter_bridge',
         votes: 16,
         created_at: 975242163389
       }
     ];
     const refObj = {
       A: 1,
-      "Living in the shadow of a great man": 2,
+      'Living in the shadow of a great man': 2,
       "They're not exactly dogs, are they?": 3
     };
     const actual = formatComments(comments, refObj);
     expect(actual[2].created_at).to.eql(new Date(975242163389));
   });
-  it("does not mutate the passed array of comments objects", () => {
+  it('does not mutate the passed array of comments objects', () => {
     const comments = [
       {
-        body: "This is a bad article name",
-        belongs_to: "A",
-        created_by: "butter_bridge",
+        body: 'This is a bad article name',
+        belongs_to: 'A',
+        created_by: 'butter_bridge',
         votes: 1,
         created_at: 1038314163389
       },
       {
-        body: "The owls are not what they seem.",
+        body: 'The owls are not what they seem.',
         belongs_to: "They're not exactly dogs, are they?",
-        created_by: "icellusedkars",
+        created_by: 'icellusedkars',
         votes: 20,
         created_at: 1006778163389
       },
       {
-        body: "This morning, I showered for nine minutes.",
-        belongs_to: "Living in the shadow of a great man",
-        created_by: "butter_bridge",
+        body: 'This morning, I showered for nine minutes.',
+        belongs_to: 'Living in the shadow of a great man',
+        created_by: 'butter_bridge',
         votes: 16,
         created_at: 975242163389
       }
     ];
     const refObj = {
       A: 1,
-      "Living in the shadow of a great man": 2,
+      'Living in the shadow of a great man': 2,
       "They're not exactly dogs, are they?": 3
     };
     formatComments(comments, refObj);
     expect(comments).to.eql([
       {
-        body: "This is a bad article name",
-        belongs_to: "A",
-        created_by: "butter_bridge",
+        body: 'This is a bad article name',
+        belongs_to: 'A',
+        created_by: 'butter_bridge',
         votes: 1,
         created_at: 1038314163389
       },
       {
-        body: "The owls are not what they seem.",
+        body: 'The owls are not what they seem.',
         belongs_to: "They're not exactly dogs, are they?",
-        created_by: "icellusedkars",
+        created_by: 'icellusedkars',
         votes: 20,
         created_at: 1006778163389
       },
       {
-        body: "This morning, I showered for nine minutes.",
-        belongs_to: "Living in the shadow of a great man",
-        created_by: "butter_bridge",
+        body: 'This morning, I showered for nine minutes.',
+        belongs_to: 'Living in the shadow of a great man',
+        created_by: 'butter_bridge',
         votes: 16,
         created_at: 975242163389
       }
     ]);
   });
 });
-describe("formatPostedComment", () => {
-  it("returns a new empty object if passed an empty object", () => {
+describe('formatPostedComment', () => {
+  it('returns a new empty object if passed an empty object', () => {
     const postedComment = {};
     const article_id = 3;
     const actual = formatPostedComment(postedComment, article_id);
@@ -408,24 +407,24 @@ describe("formatPostedComment", () => {
   });
   it("returns a new comment object with the 'username' key changed to 'author' and the article_id added", () => {
     const postedComment = {
-      username: "icellusedkars",
-      body: "This is a really great article!"
+      username: 'icellusedkars',
+      body: 'This is a really great article!'
     };
     const article_id = 3;
     const actual = formatPostedComment(postedComment, article_id);
-    expect(actual).to.have.keys("author", "body", "article_id");
+    expect(actual).to.have.keys('author', 'body', 'article_id');
     expect(actual.article_id).to.equal(3);
   });
-  it("does not mutate the input", () => {
+  it('does not mutate the input', () => {
     const postedComment = {
-      username: "icellusedkars",
-      body: "This is a really great article!"
+      username: 'icellusedkars',
+      body: 'This is a really great article!'
     };
     const article_id = 3;
     formatPostedComment(postedComment, article_id);
     expect(postedComment).to.eql({
-      username: "icellusedkars",
-      body: "This is a really great article!"
+      username: 'icellusedkars',
+      body: 'This is a really great article!'
     });
   });
 });
